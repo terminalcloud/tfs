682b04b5c201eabeb119cfe00613db68da8e2529
Jonathan Reem <jonathan.reem@gmail.com>
2016-03-03 19:10:09 -0800

    Have travis run tfs-fuse-sys tests as well.

--- ci/script.sh
+++ ci/script.sh
@@ -6,6 +6,12 @@ set -ex
 cargo build --target $TARGET
 cargo test --target $TARGET
 
+# Build and test tfs-fuse-sys as well
+pushd tfs-fuse-sys
+cargo build --target $TARGET
+cargo test --target $TARGET
+popd
+
 # If we are nightly run benchmarks.
 if rustc --version | grep nightly; then
     cargo bench --features nightly --target $TARGET
